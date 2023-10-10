a96bf32c0f34733f64da9c0935c8d425197257b0
ethan <ethan.credo@gmail.com>
2023-10-10 16:58:45 +0800

    solve exercises from 1 to 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let string = '';
 
+    if(0 > times) { return "ERROR"; }
+
+    for(let repeat = 0; repeat < times; repeat++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let finalSum = 0;
+
+    if(start < 0 || end < 0) { return "ERROR"; }
+    if(!Number.isInteger(start) || !Number.isInteger(end)) { return "ERROR"; }
+
+    if(start < end) {
+        for(start; start <= end; start++) {
+            finalSum += start;
+            
+        }
+        return finalSum;
+    } else if(start > end) {
+        for(start; end <= start; start--) {
+            finalSum += start;
+            
+        }
+        return finalSum;
+    }
 
 };
 
